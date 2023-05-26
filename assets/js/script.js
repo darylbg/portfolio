c22016253538ed209ba9a52617eafce3d22209df
darylbg <darylxcuf@gmail.com>
2023-05-26 11:49:40 +0100

    added typwriter animations

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
-console.log('hello world');
+const introNameEl = document.getElementById('home-name');
+const introSubtitleEl = document.getElementById('home-subtitle');
+
+setTimeout(function() {
+    introNameEl.classList.add('visible')
+    },
+1500);
+
+setTimeout(function() {
+    introSubtitleEl.classList.add('visible')
+    },
+3400);
